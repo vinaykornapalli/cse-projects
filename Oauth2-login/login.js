d9e31e9b4de45a0431d415242d7e411d5d5e7372
vinaykornapalli <vinaykornapalli@gmail.com>
2018-03-05 20:38:24 +0530

    css2

--- Oauth2-login/login.js
+++ Oauth2-login/login.js
@@ -1,8 +1,15 @@
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+    $("#info").css('display',"block")
     $("#username").text(profile.getName());
     $("#image").attr('src',profile.getImageUrl());
     $("#email").text(profile.getEmail());
   }
   
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      $("#loggedout").css("display" , "block");
+    });
+  }

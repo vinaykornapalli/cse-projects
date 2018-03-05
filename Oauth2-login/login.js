3ec9f3238dae2adc2c60a4d0327f375171f90bd1
vinaykornapalli <vinaykornapalli@gmail.com>
2018-03-05 20:50:09 +0530

    signout

--- Oauth2-login/login.js
+++ Oauth2-login/login.js
@@ -11,5 +11,7 @@ function onSignIn(googleUser) {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       $("#loggedout").css("display" , "block");
+      $(".g-signin2").css("display" , "block");
+      $("#info").css("display" , "none");
     });
   }
